9d72a74e35ef035ad489bf0c9267375f7de7ded1
winterlightning <raycornellwang@gmail.com>
2012-11-18 04:28:05 -0500

    tried to fix tap not working

--- js/jquery.flips.js
+++ js/jquery.flips.js
@@ -566,7 +566,9 @@
 				
 			} );
 			
-			this.$flipPages.find( '.box' ).on( 'click.flips', function( event ) {
+			this.$flipPages.find( '.box' ).live("tap", function(event) {} );
+			
+			this.$flipPages.find( '.box' ).on( 'click.flips tap.flips', function( event ) {
 				
 				var $box 			= $(this),
 					$boxClose		= $( '<span class="box-close">close</span>' ),
